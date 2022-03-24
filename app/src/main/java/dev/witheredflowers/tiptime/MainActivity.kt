9467ab7f9c0367b803e00da6cdbca4e01d2ef738
Withered Flowers <withered-flowers@users.noreply.github.com>
2022-03-24 21:47:20 +0700

    feat: change build.gradle to use viewBinding and create binder in MainActivity.kt

--- app/src/main/java/dev/witheredflowers/tiptime/MainActivity.kt
+++ app/src/main/java/dev/witheredflowers/tiptime/MainActivity.kt
@@ -2,10 +2,18 @@ package dev.witheredflowers.tiptime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dev.witheredflowers.tiptime.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+	
+	// lateinit adalah perjanjian bahwa kode kita akan menginisialisasi variabel sebelum digunakan
+	// kalau tidak atau lupa diinisialisasi maka aplikasi akan crash
+	lateinit var binding: ActivityMainBinding
+	
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
-		setContentView(R.layout.activity_main)
+		// setContentView(R.layout.activity_main)
+		binding = ActivityMainBinding.inflate(layoutInflater)
+		setContentView(binding.root)
 	}
 }
